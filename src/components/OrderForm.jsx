4857a871a23a88d5c202f760b52402084dd8cdd5
christoller <tollernz@gmail.com>
2023-12-05 21:08:48 +1100

    Add product images and tidy up

--- src/components/OrderForm.jsx
+++ src/components/OrderForm.jsx
@@ -10,84 +10,115 @@ import {
     CardContent,
     CardActions,
 } from '@mui/material';
-import { InputNumber } from 'rsuite';
+import '../OrderForm.css';
 
 const customerName = 'entry.1623023882';
 const deliveryDay = 'entry.501096647';
 const notes = 'entry.1874860549';
+function importAll(r) {
+    let images = {};
+    r.keys().map((item, index) => {
+        images[item.replace('./', '')] = r(item);
+    });
+    return images;
+}
+
+const images = importAll(
+    require.context('../images', false, /\.(png|jpe?g|svg)$/)
+);
+
+console.log(images);
 
 let products = {
     bananaBread: {
         productName: 'Banana Bread',
         productCode: 'entry.1421270729',
+        productImage: 'IMG_7716.jpg',
     },
     cookie: {
         productName: 'Salted Chocolate Chip Cookie',
         productCode: 'entry.374962814',
+        productImage: 'no_image.jpg',
     },
     passionfruitPoppyseed: {
         productName: 'Passionfruit Poppyseed Cake',
         productCode: 'entry.194879030',
+        productImage: 'IMG_7713.jpg',
     },
     carrotCake: {
         productName: 'Carrot Cake',
         productCode: 'entry.639809459',
+        productImage: 'IMG_7583.jpg',
     },
     lemonBlueberry: {
         productName: 'Lemon Blueberry Cake',
         productCode: 'entry.134686276',
+        productImage: 'no_image.jpg',
     },
     lemonTart: {
         productName: 'Lemon Tart',
         productCode: 'entry.1348942672',
+        productImage: 'IMG_7714.jpg',
     },
     brownie: {
         productName: 'Chocolate Brownie',
         productCode: 'entry.868556964',
+        productImage: 'no_image.jpg',
     },
     orangeCake: {
         productName: 'Orange Flourless Cake',
         productCode: 'entry.783553071',
+        productImage: 'IMG_7474.jpg',
     },
     baconEgg: {
         productName: 'Bacon & Egg Pie',
         productCode: 'entry.1142617283',
+        productImage: 'IMG_7677.jpg',
     },
     beefPie: {
         productName: 'Beef Pie',
         productCode: 'entry.128124',
+        productImage: 'IMG_7547.jpg',
     },
     mushroomPie: {
         productName: 'Mushroom & Leek Pie',
         productCode: 'entry.304859206',
+        productImage: 'no_image.jpg',
     },
     sausageRoll: {
         productName: 'Pork & Fennel Sausage Roll',
         productCode: 'entry.1566016836',
+        productImage: 'no_image.jpg',
     },
     croissant: {
         productName: 'Croissant',
         productCode: 'entry.120012199',
+        productImage: 'no_image.jpg',
     },
     almondCroissant: {
         productName: 'Almond Croissant',
         productCode: 'entry.1875802636',
+        productImage: 'no_image.jpg',
     },
     danish: {
         productName: 'Fruit Danish',
         productCode: 'entry.1156254484',
+        productImage: 'no_image.jpg',
     },
     cinnamonScroll: {
         productName: 'Cinnamon Scroll',
         productCode: 'entry.1452293651',
+        productImage: 'no_image.jpg',
     },
     vegemiteScroll: {
         productName: 'Vegemite Scroll',
         productCode: 'entry.516476465',
+        productImage: 'no_image.jpg',
     },
     babka: {
         productName: 'Babka (Fridays Only)',
         productCode: 'entry.759466499',
+        productImage: 'no_image.jpg',
     },
 };
 
@@ -121,34 +152,62 @@ export const OrderForm = () => {
                     flexDirection='column'
                     justifyContent='center'
                     alignItems='center'>
-                    <label htmlFor={customerName} placeholder='Your Name'>
-                        Ordered By
-                    </label>
-                    <br />
-                    <TextField
-                        id='outlined-basic'
-                        variant='outlined'
-                        type='text'
-                        name={customerName}
-                    />
-                    <br />
-                    <label htmlFor={deliveryDay}>Delivery Day</label>
-                    <br />
-                    <TextField
-                        id='outlined-basic'
-                        variant='outlined'
-                        type='date'
-                        onChange={(e) => {
-                            setDeliveryDate(e.target.value);
-                        }}
-                        name={deliveryDay}
-                        value={deliveryDate}
-                        required={true}
-                    />
-                    <br />
+                    <Card
+                        sx={{
+                            minWidth: 400,
+                            mt: 1.5,
+                        }}>
+                        <CardContent>
+                            <Typography
+                                htmlFor={customerName}
+                                placeholder='Your Name'>
+                                Ordered By
+                            </Typography>
+                            <TextField
+                                id='outlined-basic'
+                                variant='outlined'
+                                type='text'
+                                name={customerName}
+                            />
+                        </CardContent>
+                    </Card>
+                    <Card sx={{ minWidth: 400, mt: 1.5 }}>
+                        <CardContent>
+                            <Typography htmlFor={deliveryDay}>
+                                Delivery Day
+                            </Typography>
+                            <TextField
+                                id='outlined-basic'
+                                variant='outlined'
+                                type='date'
+                                onChange={(e) => {
+                                    setDeliveryDate(e.target.value);
+                                }}
+                                name={deliveryDay}
+                                value={deliveryDate}
+                                required={true}
+                            />
+                        </CardContent>
+                    </Card>
                     {Object.keys(products).map((product, index) => (
-                        <Card sx={{ minWidth: 275 }}>
-                            <CardContent>
+                        <Card sx={{ minWidth: 400, mt: 1.5 }}>
+                            <CardContent
+                                sx={{
+                                    display: 'flex',
+                                    justifyContent: 'center',
+                                    flexDirection: 'column',
+                                    alignItems: 'center',
+                                }}>
+                                <img
+                                    src={images[products[product].productImage]}
+                                    alt={images[products[product].productName]}
+                                    width={100}
+                                    height={100}
+                                    style={{
+                                        borderRadius: '50%',
+                                        objectFit: 'cover',
+                                    }}
+                                />
                                 <Typography
                                     htmlFor={products[product].productCode}>
                                     {products[product].productName}
@@ -175,6 +234,13 @@ export const OrderForm = () => {
                                             pattern='\d*'
                                             name={products[product].productCode}
                                             placeholder='0'
+                                            InputProps={{
+                                                inputProps: {
+                                                    style: {
+                                                        textAlign: 'center',
+                                                    },
+                                                },
+                                            }}
                                         />
                                         <Button
                                             onClick={() => {
@@ -204,15 +270,21 @@ export const OrderForm = () => {
                         </div> */}
                         </Card>
                     ))}
-                    <label htmlFor={notes}>Additional Notes </label>
-                    <br />
-                    <TextField
-                        id='outlined-basic'
-                        variant='outlined'
-                        type='text'
-                        name={notes}
-                    />
-                    <br />
+                    <Card sx={{ minWidth: 400, mt: 1.5 }}>
+                        <CardContent>
+                            <Typography htmlFor={notes}>
+                                Additional Notes
+                            </Typography>
+                            <TextField
+                                id='outlined-basic'
+                                variant='outlined'
+                                type='text'
+                                name={notes}
+                                multiline
+                                minRows={3}
+                            />
+                        </CardContent>
+                    </Card>
                     <Button type='submit' variant='contained'>
                         Submit
                     </Button>
---
+++ src/images.js
@@ -0,0 +1,4 @@
+export { default as image1 } from 'assets/images/1.png';
+export { default as image2 } from 'assets/images/2.png';
+export { default as image3 } from 'assets/images/3.png';
+export { default as image4 } from 'assets/images/4.png';
